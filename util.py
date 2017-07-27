bf617a98717913942bb616c9326c9a6d29534775
caokun <caokun@xfz.cn>
2017-07-27 11:29:11 +0800

    添加 dict to xml  和  xml to dict 的公共方法

---
+++ util.py
@@ -0,0 +1,65 @@
+# /usr/bin/env python
+# coding=utf8
+
+
+import json
+import xmltodict
+
+
+XML_STR = """
+<student> 
+    <stid>10213</stid> 
+    <info> 
+        <name>name</name> 
+        <mail>xxx@xxx.com</mail> 
+        <sex>male</sex> 
+    </info> 
+    <course> 
+        <name>math</name> 
+        <score>90</score> 
+    </course> 
+    <course> 
+        <name>english</name> 
+        <score>88</score> 
+    </course> 
+</student>
+"""
+
+
+def xml_to_dict(xml_str):
+    """
+    xml字符串转dict
+    :param xml_str:
+    :return: demo_dict
+    :rtype: dict
+    """
+    try:
+        order_tuple = xmltodict.parse(xml_str)
+        demo_dict_str = json.dumps(order_tuple)
+        demo_dict = json.loads(demo_dict_str)
+    except Exception as e:
+        print e
+        demo_dict = dict()
+    return demo_dict
+
+
+def dict_to_xml(info_dict):
+    """
+    dict 转 xml
+    :param info_dict:
+    :return: xml_str
+    :rtype: str
+    """
+    try:
+        xml_str = xmltodict.unparse(info_dict)
+    except Exception as e:
+        print e
+        xml_str = ''
+    return xml_str
+
+
+if __name__ == '__main__':
+    demo_dict = xml_to_dict(XML_STR)
+    print demo_dict
+    xml_str = dict_to_xml(demo_dict)
+    print xml_str
